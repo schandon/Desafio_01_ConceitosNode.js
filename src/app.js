1e6e175d61ca345a5c96a253143243c86420ec46
Alexandre Pereira <alexandre.pereira@jrc.rio.br>
2021-11-19 16:09:43 -0300

    Teste 02

--- src/app.js
+++ src/app.js
@@ -14,22 +14,38 @@ app.get("/repositories/", (request, response) => {
 });
 
 app.post("/repositories/", (request, response) => {
-  const { title, url, techs} = request.body;
-  const project = { id: uuid, title, url, techs};
+  const { title, url, techs } = request.body;
+  const project = { id: uuid(), title, url, techs, likes: 0};
   repositories.push(project);
-  return response.json(project);
+  return response.status(204).json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  if(!isUuid(id)){
+    return response.status(400).json({error : "Project not Found."})
+  }
+  const project = repositories.findIndex(repository => repository.id === id);
+  project.push(title,url,techs);
+  return response.status(201).json(project);
 });
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/likes", (request, response) => {
+  const { id } = request.params;
+  const { likes } = request.body;
+  const repository = repositories.find( repository => repository.id === id);
+  if( !repository){
+    return response.status(400).json();
+  }
+  console.log(repository);
+  repository.likes+= likes;
+  console.log(repository);
+  return response.json(repository);
 });
 
 module.exports = app;
